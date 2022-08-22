4e612217f53da2cd40df89981cb110a89e1aece3
SpoodyAlt <100691170+SpoodyAlt@users.noreply.github.com>
2022-08-22 16:59:05 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+console.log("Yo k1lla, dont judge im better, Swift, stop dumping my Websites, yo vyast i aint copy you bitch")
+
+function OpenGithub() {
+  window.open("https://github.com/SpoodyDev", "_blank");
+}
+
+function OpenDiscord() {
+  window.open("https://discord.com/invite/spoody", "_blank");
+}
+
+function OpenShop() {
+  window.open("https://spoodydev.tebex.io", "_blank");
+}
+
+function OpenRaven() {
+  window.open("https://spoody.dev/RavenAC", "_blank");
+}
